14fc22a01d26365cedf7ea12876f5b24e8d51b19
Brett Randall <brett.randall@confluent.io>
2021-10-15 08:31:31 +1100

    Moved provider version constraints into a required_providers block.
    Resolves deprecation warning.

--- privatelink/aws/terraform/privatelink.tf
+++ privatelink/aws/terraform/privatelink.tf
@@ -1,10 +1,15 @@
 terraform {
   required_version = ">= 0.13.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 2.32.0"
+    }
+  }
 }
 
 provider "aws" {
   region  = var.region
-  version = "= 2.32.0"
 }
 
 
