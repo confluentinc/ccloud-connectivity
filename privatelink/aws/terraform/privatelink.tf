9153d6f189bfefe752fbc5cc1ffdf0ab46e99045
anjvenkat <84352911+anjvenkat@users.noreply.github.com>
2022-10-07 09:51:24 -0700

    Clusters deployed with private dns enabled does not have `glb` in the bootstrap url. The regex to retrieve the zone id needs to be adjusted a bit for private dns enabled bootstrap URLs.
    For now, focusing on aws changes.  We will make the changes to Azure and GCP in the next round of changes.

--- privatelink/aws/terraform/privatelink.tf
+++ privatelink/aws/terraform/privatelink.tf
@@ -39,7 +39,7 @@ variable "subnets_to_privatelink" {
 }
 
 locals {
-  hosted_zone = replace(regex("^[^.]+-([0-9a-zA-Z]+[.].*):[0-9]+$", var.bootstrap)[0], "glb.", "")
+  hosted_zone = length(regexall(".glb", var.bootstrap)) > 0 ? regex("^[^.]+-([0-9a-zA-Z]+[.].*):[0-9]+$", var.bootstrap)[0] : regex("[.]([0-9a-zA-Z]+[.].*):[0-9]+$", var.bootstrap)[0]
 }
 
 
