147cc20f55f5653db204fbc5027c38d6429ad2e7
anjvenkat <84352911+anjvenkat@users.noreply.github.com>
2022-12-07 15:59:04 -0800

    Clusters deployed with private dns enabled do not have glb in the bootstrap url (#37)

--- privatelink/gcp/terraform/privatelink.tf
+++ privatelink/gcp/terraform/privatelink.tf
@@ -52,7 +52,7 @@ variable "psc_service_attachments_by_zone" {
 }
 
 locals {
-  hosted_zone = replace(regex("^[^.]+-([0-9a-zA-Z]+[.].*):[0-9]+$", var.bootstrap)[0], "glb.", "")
+  hosted_zone = length(regexall(".glb", var.bootstrap)) > 0 ? replace(regex("^[^.]+-([0-9a-zA-Z]+[.].*):[0-9]+$", var.bootstrap)[0], "glb.", "") : regex("[.]([0-9a-zA-Z]+[.].*):[0-9]+$", var.bootstrap)[0]
   network_id  = regex("^([^.]+)[.].*", local.hosted_zone)[0]
 }
 
