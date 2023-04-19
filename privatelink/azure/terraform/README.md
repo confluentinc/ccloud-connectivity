525e0abd10faf2f03ee74488a30e31d02f80ffb5
Venky Narayanan <venkyraghav@gmail.com>
2023-04-19 14:50:45 -0400

    Added Azure PrivateLinkAccess (#32)

--- privatelink/azure/terraform/README.md
+++ privatelink/azure/terraform/README.md
@@ -1,11 +1,59 @@
 # privatelink/azure/terraform
 
+Configure Azure Terraform Connectivity
+
+Configure Confluent Cloud API Key and Secret
+
+    export TF_VAR_CONFLUENT_CLOUD_API_KEY=<APIKEY>
+    export TF_VAR_CONFLUENT_CLOUD_API_SECRET=<SECRET>
+    export CONFLUENT_CLOUD_API_KEY=${TF_VAR_CONFLUENT_CLOUD_API_KEY}
+    export CONFLUENT_CLOUD_API_SECRET=${TF_VAR_CONFLUENT_CLOUD_API_SECRET}
+
 Use the supplied `terraform.tfvars` file to supply required parameters to
-setup your VNET for Confluent Cloud Private Link.
+    
+    # Resource tags
+    owner_email    = "youremail@company.com"
+    purpose        = "WhyAreYouCreatingThisInfrastructure"
+
+    # Subscription ID
+    subscription   = "your-azure-subscription"
+    # Azure Region
+    region         = "azure-region"
+    # Azure Resource Group
+    resource_group = "AzureResourceGroup"
+    #Azure VNET Name
+    vnet_name      = "AzureVNET"
+
+    # Confluent Environment to use
+    env_name       = "EnvironmentDisplayName"
+    # Confluent Dedicated Cluster to create
+    cluster_name   = "ClusterDisplayName"
+    # CKU count of the dedicated cluster
+    cluster_cku    = CKUCount
+    # Confluent Private Link Network Name
+    network_name   = "NetworkDisplayName"
+
+    # KSQL Cluster Name
+    ksql_cluster   = "KSQLClusterName"
+    # App ID for KSQL cluster. will be provisioning with ClusterAdmin role
+    ksql_app_id    = "AppID"
+
+Lifecycle parameter is commented out now. To prevent accidentally destroying resource, uncomment the block on the resource
+    
+    # Uncomment if you don't want cluster to be destroyed
+    # lifecycle {
+    #  prevent_destroy = true
+    # }
 
 After populating it, simply run terraform (https://www.terraform.io/):
 
     terraform init
-    terraform apply
+    terraform apply -target azurerm_private_dns_zone.hz
+    terraform apply 
 
-The `privatelink_service_alias` is provided to you directly from Confluent.
+To Destroy, use `terraform destroy`
+
+    terraform destroy -target azurerm_private_endpoint.endpoint
+    terraform destroy -target azurerm_private_dns_zone.hz
+    terraform destroy -target confluent_network.azure-private-link
+    terraform destroy 
--- privatelink/azure/terraform/privatelink.tf
+++ privatelink/azure/terraform/privatelink.tf
@@ -67,13 +67,22 @@ data "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_private_dns_zone" "hz" {
+  name = local.hosted_zone
   resource_group_name = data.azurerm_resource_group.rg.name
 
-  name = local.hosted_zone
+  tags = {
+    owner_email = "${var.owner_email}"
+    purpose = "${var.purpose}"
+  }
+
+  # Uncomment if you don't want cluster to be destroyed
+  # lifecycle {
+  #  prevent_destroy = true
+  # }
 }
 
 resource "azurerm_private_endpoint" "endpoint" {
-  for_each = var.privatelink_service_alias_by_zone
+  for_each = confluent_network.azure-private-link.azure[0].private_link_service_aliases
 
   name                = "confluent-${local.network_id}-${each.key}"
   location            = var.region
@@ -85,8 +94,18 @@ resource "azurerm_private_endpoint" "endpoint" {
     name                              = "confluent-${local.network_id}-${each.key}"
     is_manual_connection              = true
     private_connection_resource_alias = each.value
-    request_message                   = "PL"
+    request_message                   = "PL request by ${var.owner_email} for ${var.purpose}"
   }
+
+  tags = {
+    owner_email = "${var.owner_email}"
+    purpose = "${var.purpose}"
+  }
+
+  # Uncomment if you don't want cluster to be destroyed
+  # lifecycle {
+  #  prevent_destroy = true
+  # }
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "hz" {
@@ -94,6 +113,11 @@ resource "azurerm_private_dns_zone_virtual_network_link" "hz" {
   resource_group_name   = data.azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.hz.name
   virtual_network_id    = data.azurerm_virtual_network.vnet.id
+
+  # Uncomment if you don't want cluster to be destroyed
+  # lifecycle {
+  #  prevent_destroy = true
+  # }
 }
 
 resource "azurerm_private_dns_a_record" "rr" {
@@ -104,10 +128,15 @@ resource "azurerm_private_dns_a_record" "rr" {
   records             = [
     for _, ep in azurerm_private_endpoint.endpoint: ep.private_service_connection[0].private_ip_address
   ]
+
+  # Uncomment if you don't want cluster to be destroyed
+  # lifecycle {
+  #  prevent_destroy = true
+  # }
 }
 
 resource "azurerm_private_dns_a_record" "zonal" {
-  for_each = var.privatelink_service_alias_by_zone
+  for_each = azurerm_private_endpoint.endpoint
 
   name                = "*.az${each.key}"
   zone_name           = azurerm_private_dns_zone.hz.name
@@ -116,4 +145,9 @@ resource "azurerm_private_dns_a_record" "zonal" {
   records             = [
     azurerm_private_endpoint.endpoint[each.key].private_service_connection[0].private_ip_address,
   ]
+
+  # Uncomment if you don't want cluster to be destroyed
+  # lifecycle {
+  #  prevent_destroy = true
+  # }
 }
